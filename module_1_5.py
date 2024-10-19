4bf8930127807b1898e5d39621aae83f439ba288
Фируза <ferika2525@gmail.com>
2024-10-19 14:31:32 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -3,12 +3,6 @@
 #immutable_war[0] = 500
 #print(immutable_war)
 
-mutable_list = [1, 2,], ['a book', 'a box']
-print(mutable_list)
-print(mutable_list[1])
-mutable_list[1][1] = 'a book'
-print(mutable_list)
-
 mutable_list = [1, 2, 3]
 print(mutable_list)
 mutable_list.append(1)
